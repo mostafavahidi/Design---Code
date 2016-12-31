9e1e92b54cbcf5d187e8079df70e44b7a3f18b12
mostafavahidi <mostafa.vahidi@gmail.com>
2016-12-31 04:42:19 -0800

    About + Organizations Update

--- gulpfile.js
+++ gulpfile.js
@@ -44,8 +44,7 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
     browserSync({
         server: {
             baseDir: '_site'
-        },
-        notify: false
+        }
     });
 });
 
@@ -89,7 +88,9 @@ gulp.task('pug', function(){
  */
 gulp.task('watch', function () {
     gulp.watch('assets/css/**', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
+    gulp.watch('assets/js/**', ['jekyll-rebuild']);
+    gulp.watch(['index.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
+    gulp.watch(['assets/js/**'], ['jekyll-rebuild']);
     gulp.watch('_pugfiles/*.pug', ['pug']);
 });
 
